c480a5b683436f8c7823148106f9d29fcf2f12a1
Deskrel <ismaelpsy.12@gmail.com>
2023-12-05 17:04:42 -0500

    add db credentials

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ import Config
 
 # Configure your database
 config :test_api, TestApi.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "backend",
+  password: "170520210921",
   hostname: "localhost",
   database: "test_api_dev",
   stacktrace: true,
